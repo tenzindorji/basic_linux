3147b6f94181ba3ace114dff6ad10c88c9b175d0
tdorji <tenzin_dorji@intuit.com>
2020-02-28 10:07:52 -0800

    adding sync upstream script

--- syncupstream.sh
+++ syncupstream.sh
@@ -6,7 +6,8 @@ git remote | grep upstream >> /dev/null 2>&1
 
 if [ "$?" != "0" ]; then
         printf "Adding upstream git repo\n"
-        git remote add upstream git@github.com:tenzindorji/basic_linux.git
+        #git remote add upstream git@github.com:tenzindorji/basic_linux.git
+        git remote add upstream https://github.com/tenzindorji/basic_linux.git
 else
         printf "\n"
 fi
